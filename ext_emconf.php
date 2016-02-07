4dd40b7d359e02bf4c19d17930a0733ed34577b3
Oliver Klee <typo3-coding@oliverklee.de>
2016-02-07 17:53:55 +0100

    [FEATURE] Add the extension basics

---
+++ ext_emconf.php
@@ -0,0 +1,51 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "oelib".
+ *
+ * Auto generated 06-01-2015 19:56
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'TYPO3 CMS development site',
+	'description' => 'This is a TYPO3 extension that contains the basics of a site which I use for developing in TYPO3.',
+	'category' => 'services',
+	'author' => 'Oliver Klee',
+	'author_email' => 'typo3-coding@oliverklee.de',
+	'shy' => 0,
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'author_company' => 'oliverklee.de',
+	'version' => '1.0.0',
+	'_md5_values_when_last_written' => '',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '5.5.0-7.0.99',
+			'typo3' => '6.2.0-7.9.99'
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+	'autoload' => array(
+	),
+);
