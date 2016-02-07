678b2cfd8e2abd5bd44138b40caa197f5762c97b
Oliver Klee <typo3-coding@oliverklee.de>
2016-02-07 18:17:49 +0100

    [TASK] Add a TS template and fluid templates

---
+++ ext_tables.php
@@ -0,0 +1,8 @@
+<?php
+defined('TYPO3_MODE') or die('Access denied.');
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+  $_EXTKEY,
+  'Configuration/TypoScript',
+  'TYPO3 development site'
+);
