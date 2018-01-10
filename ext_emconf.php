3441169a69e94c20cc3d8e01b95ee506770abbfc
Oliver Klee <typo3-coding@oliverklee.de>
2018-01-10 21:43:36 +0100

    [TASK] Only allow TYPO3 up to 8.7.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '3.3.6',
     'constraints' => [
         'depends' => [
-            'typo3' => '6.2.0-8.9.99'
+            'typo3' => '6.2.0-8.7.99'
         ],
         'conflicts' => [
         ],
