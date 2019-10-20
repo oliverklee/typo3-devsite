10b6c7b247848e5a1b05c9cd36ad30962e21b80e
Oliver Klee <typo3-coding@oliverklee.de>
2019-10-20 13:32:24 +0200

    [TASK] Require fluid and fluid_style_content

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,10 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '7.0.0-7.9.99',
-            'typo3' => '7.6.0-8.7.99'
+            'typo3' => '7.6.0-8.7.99',
+            'frontend' => '7.6.0-8.7.99',
+            'fluid' => '7.6.0-8.7.99',
+            'fluid_styled_content' => '7.6.0-8.7.99',
         ],
         'conflicts' => [
         ],
