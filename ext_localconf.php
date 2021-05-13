4ddd31fd6695b284c39bdc0321d420f3beb28de8
Oliver Klee <github@oliverklee.de>
2021-05-13 18:02:45 +0200

    [TASK] Disabled unused backend columns (#84)
    Fixes #53

---
+++ ext_localconf.php
@@ -0,0 +1,5 @@
+<?php
+
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    '<INCLUDE_TYPOSCRIPT: source="DIR: EXT:typo3_devsite/Configuration/TSconfig/" extensions="tsconfig">'
+);
