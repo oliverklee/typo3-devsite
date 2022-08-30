857b4528289b0589a5c1d4c3cc6aacc15b558787
Oliver Klee <typo3-coding@oliverklee.de>
2022-08-30 18:18:54 +0200

    [CLEANUP] Use CamelCase for the `TsConfig` folder (#139)
    This make the casing consistent with the TYPO3 core.
    
    Fixes #132

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,5 @@
 <?php
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="DIR: EXT:typo3_devsite/Configuration/TSconfig/" extensions="tsconfig">'
+    '<INCLUDE_TYPOSCRIPT: source="DIR: EXT:typo3_devsite/Configuration/TsConfig/" extensions="tsconfig">'
 );
