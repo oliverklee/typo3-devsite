2653fdb232b2f388df1962e8b6486af47513313d
Oliver Klee <typo3-coding@oliverklee.de>
2023-04-08 16:55:42 +0200

    [FEATURE] Add configuration to show the page UIDs in the page tree

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,3 +3,6 @@
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
     '@import "EXT:typo3_devsite/Configuration/TsConfig/Page/"'
 );
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
+    '@import "EXT:typo3_devsite/Configuration/TsConfig/User/"'
+);
