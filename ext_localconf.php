77554a32be692f1047db1fed7a232527f41a55ca
Oliver Klee <typo3-coding@oliverklee.de>
2023-04-08 16:52:54 +0200

    [CLEANUP] Streamline the TsConfig setup
    Make the setup consistent with other site setups, and also allow having
    User TsConfig as well.
    
    Also switch to the `@import` syntax.

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,5 +1,5 @@
 <?php
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="DIR: EXT:typo3_devsite/Configuration/TsConfig/" extensions="tsconfig">'
+    '@import "EXT:typo3_devsite/Configuration/TsConfig/Page/"'
 );
